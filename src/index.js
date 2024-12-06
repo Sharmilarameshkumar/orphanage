3e7318c62ad43d7911d7bf7671afe993d88c4274
Sharmilarameshkumar <146513940+Sharmilarameshkumar@users.noreply.github.com>
2024-12-06 09:37:06 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Create the root
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // Temporarily comment out StrictMode to see if it resolves the issue
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Measure app performance (optional)
+reportWebVitals(console.log);
+
